88585a2973dba1b11fc72939a44398760ef315ee
NWChen <neilwchen@gmail.com>
2025-04-12 23:45:58 -0400

    deploy to vercel not gh pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,11 @@
 import type { AppProps } from 'next/app'
 import React from 'react';
 import './styles.css';
-import Script from 'next/script';
+import { Analytics } from "@vercel/analytics/react"
 
 function App({ Component, pageProps }: AppProps) {
   return <>
-    {/* Google tag (gtag.js) */}
-    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TN3LFVWBMK"></Script>
-    <Script>
-      {`window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-TN3LFVWBMK');`}
-    </Script>
+    <Analytics />
     <Component {...pageProps} />
   </>
 }
