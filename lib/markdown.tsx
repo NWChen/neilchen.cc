718bfd071a7b6193b347feed40e11a6cdc4d8e18
NWChen <neilwchen@gmail.com>
2025-06-22 08:47:00 -0400

    bump image size

--- lib/markdown.tsx
+++ lib/markdown.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const renderers = {
   image: ({ ...props })  => 
     <img style={{ 
-      maxWidth: '100%',
+      maxWidth: '75%',
       display: 'block',
       margin: '0 auto'
     }} {...props}/>
